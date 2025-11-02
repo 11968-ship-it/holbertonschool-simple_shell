82c39351faaa2950d5d223ed81c4fb1483928a00
Yara Alrasheed <11982@holbertonstudents.com>
2025-11-02 18:33:32 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+#include "shell.h"
 
+/**
+* main - Simple shell first step.
+* Return: Always 0
+*/
+
+int main(void)
+{
+  if (isatty(STDIN_FILENO))
+  write(STDOUT_FILENO, "($) ", 4);
+
+return (0);
+}
