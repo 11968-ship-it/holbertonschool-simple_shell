d6143a94decb53f900ccde53a875bb988686fad6
11968-ship-it <11968@holbertonstudents.com>
2025-11-02 18:44:22 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,30 @@
 #include "shell.h"
 
 /**
-* main - Simple shell first step.
+* main - Simple shell: promt + read 
 * Return: Always 0
 */
 
 int main(void)
 {
-  if (isatty(STDIN_FILENO))
-  write(STDOUT_FILENO, "($) ", 4);
+char *line = NULL;
+size_t len = 0;
+ssize_t nread;
 
+while (1)
+{
+
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "($) ", 4);
+nread = getline(&line, &len, stdin);
+if (nread == -1)
+{
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "\n", 1);
+break;
+}
+write(STDOUT_FILENO, line, nreade);
+}
+free(line);
 return (0);
 }
