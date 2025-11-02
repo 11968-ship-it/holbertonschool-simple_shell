a1f1664d2919e534acbd6750d7a2e3a5cb512ed7
Yara Alrasheed <11982@holbertonstudents.com>
2025-11-02 18:31:32 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,1 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#define BUFFER SIZE = 1024
+
+/* FUNCTION PROTOTYPES */
+
+
+
+#endif
