b9420c9fcbe1d889a930bbca0735d657954c0778
Yara Alrasheed <11982@holbertonstudents.com>
2025-11-03 18:04:46 +0300

    feat: implement simple shell with basic command execution
    - Display prompt (#cisfun$) and read user input
    - Fork child processes to execute commands with execve
    - Handle end-of-file (Ctrl+D) and errors
    - Use system calls write, fork, execve, and wait

--- main.c
+++ main.c
@@ -11,12 +11,10 @@ int main(void)
 {
 char *line = NULL;
 size_t len = 0;
-/*عدد الأحرف الللي بيقرأها gitline*/
 ssize_t nread;
-/*
 pid_t child_pid;
 char *argv[2];
-*/
+
 /*هنا حلقة تدور إلى ما لا نهاية*/
 while (1)
 {
@@ -37,24 +35,7 @@ if (line[nread - 1] == '\n')
 	line[nread -1] = '\0';
 /*هنا نحط عملية النسخ نفس قصة أمس*/
 
-
-}
-free(line);
-return (0);
-}
-/*if (isatty(STDIN_FILENO))
-write(STDOUT_FILENO, "($) ", 4);
-nread = getline(&line, &len, stdin);
-if (nread == -1)
-{
-if (isatty(STDIN_FILENO))
-write(STDOUT_FILENO, "\n", 1);
-break;
-}
-
-if (line[nread -1] == '\n')
-line[nread - 1] = '\0';
-
+	/* نبدا في الفورك */
 child_pid = fork();
 if (child_pid == -1)
 {
@@ -66,7 +47,8 @@ if (child_pid == 0)
 {
 	argv[0] = line;
 	argv[1] = NULL;
-	if (execve(argv[0], argv, NULL) == -1)
+	/*changed NULL to environ*/
+	if (execve(argv[0], argv, environ) == -1)
 		perror("./hsh");
 	exit(EXIT_FAILURE);
 }
@@ -75,8 +57,6 @@ else
 	wait(NULL);
 }
 }
-/** write(STDOUT_FILENO, line, nread); 
 free(line);
 return (0);
 }
-*/
