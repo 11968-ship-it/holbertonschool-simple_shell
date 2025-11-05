fbd51bae379fd34cd7cdfbaa2d6e94b17b107017
11968-ship-it <11968@holbertonstudents.com>
2025-11-05 19:56:14 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -4,97 +4,53 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include "shell.h"
-/**
-* prompt - Prints the shell prompt
-* if input is from terminal
-*/
+
+extern char **environ;  // Needed for execve
+
 void prompt(void)
 {
-if (isatty(STDIN_FILENO))
-write(STDOUT_FILENO, "#cisfun$ ", 9);
+    if (isatty(STDIN_FILENO))
+        write(STDOUT_FILENO, "#cisfun$ ", 9);
 }
 
-/**
-* read_line - Reads a line from stdin
-* and removes the newline
-* Return: Pointer to the line
-*/
 char *read_line(void)
 {
-char *line = NULL;
-size_t len = 0;
-ssize_t nread;
-nread = getline(&line, &len, stdin);
-if (nread == -1)
-{
-free(line);
-return (NULL);
-}
-if (line[nread - 1] == '\n')
-line[nread - 1] = '\0';
-return (line);
-}
-/**
-* execute - forks and executes
-* a single-word command.
-* @argv: argument value
-*/
-void execute(char **argv, *shell_name)
-{
-pid_t child_pid;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
 
-child_pid = fork();
-if (child_pid == -1)
-{
-perror("fork");
-return;
-}
-if (child_pid == 0)
-{
-if (execve(argv[0], argv, environ) == -1)
-{
-fprintf(stderr, "%s: No such file or directory\n", shell_name);
-exit(EXIT_FAILURE);
-}
-}
-else
-wait(NULL);
-}
-/**
-* main - Simple shell that runs basic commands.
-* Return: Always 0
-*/
-int main(int argc, char **argv)
-{
-char *shell_name = argv[0];
-char *line, *token;
-char *argv[64];
-int i;
+    nread = getline(&line, &len, stdin);
+    if (nread == -1)
+    {
+        free(line);
+        return NULL;
+    }
 
-while (1)
-{
-prompt();
-line = read_line();
-if (!line)
-{
-if (isatty(STDIN_FILENO))
-write(STDOUT_FILENO, "\n", 1);
-break;
+    if (line[nread - 1] == '\n')
+        line[nread - 1] = '\0';
+
+    return line;
 }
 
-  i = 0;
-  token = strtok(line, " \t\n");
-  while (token != NULL)
-  {
-  argv[i++] = token;
-  token = strtok(NULL, " \t\n");
-  }
-  argv[i] = NULL;
+void execute(char **argv, char *shell_name)
+{
+    pid_t child_pid;
 
-  if (i > 0)
-    execute(argv, shell_name);
-  
-free(line);
-}
-return (0);
+    child_pid = fork();
+    if (child_pid == -1)
+    {
+        perror("fork");
+        return;
+    }
+
+    if (child_pid == 0)
+    {
+        if (execve(argv[0], argv, environ) == -1)
+        {
+            fprintf(stderr, "%s: No such file or directory\n", shell_name);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+        wait(NULL);
 }
