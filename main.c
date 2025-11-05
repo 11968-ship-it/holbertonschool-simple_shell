1a0b26e534136ce0c593d7df8e03efc27e444b1b
11968-ship-it <11968@holbertonstudents.com>
2025-11-05 19:55:54 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "shell.h"
+
+int main(int argc, char **argv)
+{
+    char *shell_name = argv[0];
+    char *line, *token;
+    char *argv_list[64];
+    int i;
+
+    while (1)
+    {
+        prompt();
+        line = read_line();
+        if (!line)
+        {
+            if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 1);
+            break;
+        }
+
+        i = 0;
+        token = strtok(line, " \t\n");
+        while (token != NULL)
+        {
+            argv_list[i++] = token;
+            token = strtok(NULL, " \t\n");
+        }
+        argv_list[i] = NULL;
+
+        if (i > 0)
+            execute(argv_list, shell_name);
+
+        free(line);
+    }
+
+    return 0;
+}
