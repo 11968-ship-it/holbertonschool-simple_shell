f94db3fce1184e7f5e50ba737dd5984738624285
11968-ship-it <11968@holbertonstudents.com>
2025-11-09 18:04:16 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,82 +1,80 @@
+#include "shell.h"
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
-#include "shell.h"
 
 /**
-* prompt - Prints the shell prompt
-* if input is from terminal
-*/
+ * prompt - prints shell prompt
+ */
 void prompt(void)
 {
-	if (isatty(STDIN_FILENO))
-	write(STDOUT_FILENO, "#cisfun$ ", 9);
+    if (isatty(STDIN_FILENO))
+        write(STDOUT_FILENO, "#cisfun$ ", 9);
 }
 
 /**
-* read_line - Reads a line from stdin
-* and removes the newline
-* Return: Pointer to the line
-*/
+ * read_line - reads a line and removes newline
+ * Return: malloc'd string or NULL
+ */
 char *read_line(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread = getline(&line, &len, stdin);
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
 
-	if (nread == -1)
-	{
-	free(line);
-	return (NULL);
-	}
+    nread = getline(&line, &len, stdin);
+    if (nread == -1)
+    {
+        free(line);
+        return (NULL);
+    }
 
-	if (nread > 0 && line[nread - 1] == '\n')
-	line[nread - 1] = '\0';
+    while (nread > 0 && (line[nread - 1] == '\n' || line[nread - 1] == ' '))
+        line[--nread] = '\0';
 
-	return (line);
+    return (line);
 }
 
 /**
-* execute - forks and executes
-* a single-word command.
-* @argv: argument value
-* @shell_name: name of this shell (argv[0] of the shell),
-* used in errors.
-*/
+ * execute - executes a command
+ * @argv: arguments array
+ * @shell_name: shell name (for errors)
+ */
 void execute(char **argv, const char *shell_name)
 {
-char *cmd_path;
-pid_t child_pid;
+    char *cmd_path;
+    pid_t child_pid;
 
-	if (!argv || !argv[0])
-		return;
+    if (!argv || !argv[0])
+        return;
 
-	cmd_path = find_command_path(argv[0]);
-if (!cmd_path)
-{
-fprintf(stderr, "%s: 1: %s: not found\n",
-shell_name ? shell_name : "./hsh",
-argv[0]);
-return;
-}
-child_pid = fork();
-if (child_pid == -1)
-{
-perror("fork");
-free(cmd_path);
-return;
-}
-if (child_pid == 0)
-{
-execv(cmd_path, argv);
-perror("execv");
-_exit(127);
-}
-else
-{
-wait(NULL);
-}
-	free(cmd_path);
+    cmd_path = find_command_path(argv[0]);
+    if (!cmd_path)
+    {
+        fprintf(stderr, "%s: 1: %s: not found\n",
+                shell_name ? shell_name : "./hsh",
+                argv[0]);
+        return;
+    }
+
+    child_pid = fork();
+    if (child_pid == -1)
+    {
+        perror("fork");
+        free(cmd_path);
+        return;
+    }
+
+    if (child_pid == 0)
+    {
+        execv(cmd_path, argv);
+        perror("execv");
+        _exit(127);
+    }
+    else
+        wait(NULL);
+
+    free(cmd_path);
 }
