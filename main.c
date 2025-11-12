cec16225a0503da5ccacbf5686d52e2997220fa1
root <11982@holbertonstudents.com>
2025-11-12 16:47:49 +0000

    Betty style issues fix

--- main.c
+++ main.c
@@ -86,7 +86,7 @@ static int run_shell(const char *shell_name)
         {
             while (*command == ' ')
                 command++;
-			
+
 remove_comment(command);
             argcnt = build_argv(command, argv_list,
                     (int)(sizeof(argv_list) / sizeof(argv_list[0])));
