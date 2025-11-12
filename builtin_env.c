c523c2729e9775c2fcaa60de71ba7078b16666f6
Yara Alrasheed <11982@holbertonstudents.com>
2025-11-12 19:53:27 +0300

    Update builtin_env.c

--- builtin_env.c
+++ builtin_env.c
@@ -8,80 +8,46 @@
  * @name: variable name (must be non-empty)
  * @value: variable value (may be NULL, treated as "")
  * @overwrite: 1 = overwrite existing, 0 = skip
- *
  * Return: 0 on success, -1 on failure
  */
 int _setenv(const char *name, const char *value, int overwrite)
 {
-	int i, name_len, count;
-	char *new_var, **new_env;
-	const char *val = value ? value : "";
+	int idx;
 
 	if (!name || name[0] == '\0')
 		return (-1);
-	name_len = (int)strlen(name);
 
-	for (i = 0; environ[i]; i++)
+	idx = env_find_index(name);
+	if (idx >= 0)
 	{
-		if (strncmp(environ[i], name, name_len) == 0 &&
-		    environ[i][name_len] == '=')
-		{
-			if (!overwrite)
-				return (0);
-			new_var = malloc(name_len + strlen(val) + 2);
-			if (!new_var)
-				return (-1);
-			sprintf(new_var, "%s=%s", name, val);
-			environ[i] = new_var;
+		if (!overwrite)
 			return (0);
-		}
+		return (env_replace_index(idx, name, value));
 	}
-
-	new_var = malloc(name_len + strlen(val) + 2);
-	if (!new_var)
-		return (-1);
-	sprintf(new_var, "%s=%s", name, val);
-
-	for (count = 0; environ[count]; count++)
-		;
-
-	new_env = malloc(sizeof(char *) * (count + 2));
-	if (!new_env)
-		return (-1);
-
-	for (i = 0; i < count; i++)
-		new_env[i] = environ[i];
-	new_env[count] = new_var;
-	new_env[count + 1] = NULL;
-	environ = new_env;
-	return (0);
+	return (env_append_kv(name, value));
 }
 
 /**
  * _unsetenv - Remove an environment variable.
  * @name: variable name
- *
  * Return: 0 on success, -1 on failure
+ *
+ * Note: Removing a non-existent variable is not an error.
  */
 int _unsetenv(const char *name)
 {
-	int i, j, len;
+	int i, idx;
 
 	if (!name || name[0] == '\0')
 		return (-1);
 
-	len = (int)strlen(name);
+	idx = env_find_index(name);
+	if (idx < 0)
+		return (0);
 
-	for (i = 0; environ[i]; i++)
-	{
-		if (strncmp(environ[i], name, len) == 0 &&
-		    environ[i][len] == '=')
-		{
-			for (j = i; environ[j]; j++)
-				environ[j] = environ[j + 1];
-			return (0);
-		}
-	}
+	for (i = idx; environ[i]; i++)
+		environ[i] = environ[i + 1];
+
 	return (0);
 }
 
@@ -99,7 +65,6 @@ void builtin_env(void)
 /**
  * handle_setenv_builtin - Wrapper for setenv builtin.
  * @argv: argument vector
- *
  * Return: 0 on success, 1 on failure
  */
 int handle_setenv_builtin(char **argv)
@@ -109,15 +74,16 @@ int handle_setenv_builtin(char **argv)
 		fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
 		return (1);
 	}
+
 	if (_setenv(argv[1], argv[2], 1) == 0)
 		return (0);
+
 	return (1);
 }
 
 /**
  * handle_unsetenv_builtin - Wrapper for unsetenv builtin.
  * @argv: argument vector
- *
  * Return: 0 on success, 1 on failure
  */
 int handle_unsetenv_builtin(char **argv)
@@ -127,7 +93,9 @@ int handle_unsetenv_builtin(char **argv)
 		fprintf(stderr, "Usage: unsetenv VARIABLE\n");
 		return (1);
 	}
+
 	if (_unsetenv(argv[1]) == 0)
 		return (0);
+
 	return (1);
 }
