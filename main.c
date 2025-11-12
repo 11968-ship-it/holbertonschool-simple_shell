3daf70211c41612ba66cb096b1196322f46792f5
11968-ship-it <11968@holbertonstudents.com>
2025-11-12 18:17:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -77,7 +77,7 @@ static int run_shell(const char *shell_name)
             while (*command == ' ')
                 command++;
 			
-remove_commment(command);
+remove_comment(command);
             argcnt = build_argv(command, argv_list,
                     (int)(sizeof(argv_list) / sizeof(argv_list[0])));
 
